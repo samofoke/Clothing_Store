f36b86302e3c6409bb4d41f38dfabc0d61ff0988
samofoke <samofoke@student.wethinkcode.co.za>
2020-04-28 09:44:07 +0200

    first phase of the app with collections

---
+++ clothing-app/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.style.scss';
+import Direct from '../../components/directory/direct.component';
+
+const HomePage = () => (
+    <div className='homepage'>
+      <Direct />
+    </div>
+);
+
+export default HomePage;
---
+++ clothing-app/src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignIn from '../../components/Sign-in/Sign-in.component';
+
+
+import './signin-signup.style.scss';
+
+const SigninAndSignup = () => (
+    <div className='signin-signup'>
+        <SignIn />
+    </div>
+);
+
+export default SigninAndSignup;
