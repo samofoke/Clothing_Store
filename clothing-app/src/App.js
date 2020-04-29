8616c74ce7d1e51de971facd663b8b49bf1ea871
Slimy <samofoke@student.wethinkcode.co.za>
2020-04-29 23:32:28 +0200

    adding auth on app.js

--- clothing-app/src/App.js
+++ clothing-app/src/App.js
@@ -6,18 +6,44 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SigninAndSignup from './pages/signin-signup/signin-signup.component';
 import Header from './components/header/header.component';
+import { auth } from './FireBase/firebaseutil';
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SigninAndSignup}/>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubcribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubcribeFromAuth =auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubcribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop' component={ShopPage}/>
+          <Route path='/signin' component={SigninAndSignup}/>
+        </Switch>
+      </div>
+    );
+  };
 }
 
 export default App;
