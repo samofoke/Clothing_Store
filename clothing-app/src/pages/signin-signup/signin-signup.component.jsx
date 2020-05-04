01f3183a4bec98eb3f0178d596b441915554e629
Slimy <samofoke@student.wethinkcode.co.za>
2020-05-04 15:08:27 +0200

    sign up added with css

--- clothing-app/src/pages/signin-signup/signin-signup.component.jsx
+++ clothing-app/src/pages/signin-signup/signin-signup.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import SignIn from '../../components/Sign-in/Sign-in.component';
-
+import SignUp from '../../components/sign-up/sign-up.component';
 import './signin-signup.style.scss';
 
 const SigninAndSignup = () => (
     <div className='signin-signup'>
         <SignIn />
+        <SignUp/>
     </div>
 );
 
