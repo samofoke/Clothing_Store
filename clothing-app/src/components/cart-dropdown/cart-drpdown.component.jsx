000925b3fbf492238210910323edc8e9e8c2e0af
Sabata <samofoke@student.wethinkcode.co.za>
2020-07-09 11:05:11 +0200

    styling and cart functionalities

--- clothing-app/src/components/cart-dropdown/cart-drpdown.component.jsx
+++ clothing-app/src/components/cart-dropdown/cart-drpdown.component.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
+import Cartitem from '../cart-item/cart-item.component';
+import { connect } from 'react-redux';
 import './cart-dropdown.style.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className= 'cart-items' />
+        <div className= 'cart-items'>
+            {
+                cartItems.map(cartitem => (
+                <Cartitem key={cartitem.id} item={cartitem}
+                />))
+            }
+        </div>
         <CustomButton>CHECKOUT</CustomButton>
     </div>
 );
 
-export default CartDropdown;
+const mapSate = ({cart: {cartItems}}) => ({
+    cartItems
+});
+
+export default connect(mapSate)(CartDropdown);
