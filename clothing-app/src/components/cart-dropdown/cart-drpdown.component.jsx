087ac95296d4b07d7ed4f028d203c5a5d7d4d7dd
Sabata <samofoke@student.wethinkcode.co.za>
2020-07-12 01:09:53 +0200

    added state cronstructor to drop down component

--- clothing-app/src/components/cart-dropdown/cart-drpdown.component.jsx
+++ clothing-app/src/components/cart-dropdown/cart-drpdown.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import Cartitem from '../cart-item/cart-item.component';
+import { createStructuredSelector } from 'reselect';
 import { selectCartItems } from '../../redux/cart/selectors';
 import { connect } from 'react-redux';
 import './cart-dropdown.style.scss';
@@ -18,8 +19,8 @@ const CartDropdown = ({cartItems}) => (
     </div>
 );
 
-const mapSate = state => ({
-    cartItems: selectCartItems(state)
+const mapSate = createStructuredSelector({
+    cartItems: selectCartItems
 });
 
 export default connect(mapSate)(CartDropdown);
