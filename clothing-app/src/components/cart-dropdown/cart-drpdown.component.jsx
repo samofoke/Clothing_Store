727e70dacde0c0523a58a1add0253b0556eb0c98
Sabata <samofoke@student.wethinkcode.co.za>
2020-07-13 11:20:14 +0200

    set for the checkout page

--- clothing-app/src/components/cart-dropdown/cart-drpdown.component.jsx
+++ clothing-app/src/components/cart-dropdown/cart-drpdown.component.jsx
@@ -4,18 +4,23 @@ import Cartitem from '../cart-item/cart-item.component';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems } from '../../redux/cart/selectors';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import './cart-dropdown.style.scss';
 
-const CartDropdown = ({cartItems}) => (
+const CartDropdown = ({cartItems, history}) => (
     <div className='cart-dropdown'>
         <div className= 'cart-items'>
-            {
+            {cartItems.length ? (
                 cartItems.map(cartitem => (
-                <Cartitem key={cartitem.id} item={cartitem}
-                />))
+                <Cartitem key={cartitem.id} item={cartitem}/>
+                ))
+                ): (
+                <span className='empty'>Your cart is empty</span>)
             }
         </div>
-        <CustomButton>CHECKOUT</CustomButton>
+        <CustomButton onClick={() => history.push('/checkout')}>
+            CHECKOUT
+        </CustomButton>
     </div>
 );
 
@@ -23,4 +28,4 @@ const mapSate = createStructuredSelector({
     cartItems: selectCartItems
 });
 
-export default connect(mapSate)(CartDropdown);
+export default withRouter(connect(mapSate)(CartDropdown));
---
+++ clothing-app/src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './checkout.style.scss';
+
+const checkOutPage = () => (
+    <div>
+        chechout page
+    </div>
+)
+
+export default checkOutPage;
